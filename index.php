063e13dcf5d159284c091ed96a22c0c25ae92497
Victor <vitocr_santos@hotmail.com>
2013-07-23 18:29:14 -0300

    Sorteio de Multimidia

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
 
-$nomes = array("Euler", "Diogo", "Dennis", "Java");
+$nomes = array("Victor", "Diogo", "Ney", "Java", "Arlindo", "Joao");
 $emails = array("ba.elias@gmail.com", "euler.santana@yahoo.com", "diogopri2@gmail.com", "chuckymass@gmail.com", "www.jvfm@hotmail.com", "nancibonfim@gmail.com", "caio@dcc.ufba.br");
 
-for ($i = 0; $i < 4; $i++)
-{
-   $pos = rand(1,count($nomes)) - 1;
-   
-   $msg = "</br>" . $nomes[$pos] . " " . ($i + 1) . "</br>";
-
-    unset($nomes[$pos]);
-
-    $nomes = array_values($nomes);
-   
-    echo $msg;
-}
-
-// $flag = 1 ;
-
-// while (count($nomes) > 0)
+// for ($i = 0; $i < 4; $i++)
 // {
 //    $pos = rand(1,count($nomes)) - 1;
+   
+//    $msg = "</br>" . $nomes[$pos] . " " . ($i + 1) . "</br>";
+
+//     unset($nomes[$pos]);
+
+//     $nomes = array_values($nomes);
+   
+//     echo $msg;
+// }
+
+$flag = 1 ;
+$equipe = "";
+
+while (count($nomes) > 0)
+{
+   $pos = rand(1,count($nomes)) - 1;
     
 //    if ($flag == 1)
 //    {
@@ -33,14 +34,30 @@ for ($i = 0; $i < 4; $i++)
 // 	$flag = 1;
 //    }
 
-   // $msg = "</br>" . $nomes[$pos] . " " . $carro . "</br>";
-
-//    unset($nomes[$pos]);
-
-//    $nomes = array_values($nomes);
+   if ($flag == 1)
+   {
+   		$equipe = 1;
+   		$flag = 2;
+   }
+   else if ($flag == 2)
+   {
+   		$equipe = 2;
+   		$flag = 3;
+   }
+   else 
+   {
+   		$equipe = 3;
+   		$flag = 1;
+   }   
    
-//    echo $msg;
+   $msg = "</br>" . $nomes[$pos] . " " . $equipe . "</br>";
 
-// }
+   unset($nomes[$pos]);
+
+   $nomes = array_values($nomes);
+   
+   echo $msg;
+
+}
 
 ?>
