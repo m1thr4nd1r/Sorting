f8200ab4d0df27ab47806d3efb2b359ee4b24a17
m1thr4nd1r <vitocr_santos@hotmail.com>
2013-08-01 12:42:54 -0300

    Sorteio para Artigo SO

--- index.php
+++ index.php
@@ -1,56 +1,27 @@
 <?php
 
-$nomes = array("Victor", "Diogo", "Rose", "Java", "Arlindo", "Euler");
+$nomes = array("Victor", "Theus", "Nanci", "Arlindo");
 $emails = array("ba.elias@gmail.com", "euler.santana@yahoo.com", "diogopri2@gmail.com", "chuckymass@gmail.com", "www.jvfm@hotmail.com", "nancibonfim@gmail.com", "caio@dcc.ufba.br");
 
-// for ($i = 0; $i < 4; $i++)
-// {
-//    $pos = rand(1,count($nomes)) - 1;
-   
-//    $msg = "</br>" . $nomes[$pos] . " " . ($i + 1) . "</br>";
-
-//     unset($nomes[$pos]);
-
-//     $nomes = array_values($nomes);
-   
-//     echo $msg;
-// }
-
-$flag = 1 ;
+$flag = 0 ;
 $equipe = "";
 
 while (count($nomes) > 0)
 {
    $pos = rand(1,count($nomes)) - 1;
     
-//    if ($flag == 1)
-//    {
-// 	$carro = "Heitor";
-// 	$flag = 0;
-//    }
-//    else
-//    {
-//         $carro = "Victor";
-// 	$flag = 1;
-//    }
-
-   if ($flag == 1)
-   {
-   		$equipe = 1;
-   		$flag = 2;
-   }
-   else if ($flag == 2)
-   {
-   		$equipe = 2;
-   		$flag = 3;
-   }
-   else 
-   {
-   		$equipe = 3;
-   		$flag = 1;
-   }   
+   $flag = ($flag + 1) % 5;
    
-   $msg = "</br>" . $nomes[$pos] . " " . $equipe . "</br>";
+	if ($flag == 1)
+		$equipe = "Parte 1";
+   	else if ($flag == 2)
+   		$equipe = "Parte 2";
+   	else if ($flag == 3) 
+   		$equipe = "Parte 3";
+	else if ($flag == 4)
+		$equipe = "Parte 4";
+				   
+   $msg = "</br>" . $nomes[$pos] . " - " . $equipe . "</br>";
 
    unset($nomes[$pos]);
 
