51f1dacc12d299d2a9cd691335f3676f02bf189d
Victor <vitocr_santos@hotmail.com>
2013-09-28 17:37:00 -0300

    Adding templates and structure

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+	return "chego"
