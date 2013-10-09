a730d7fac892a70af173f2e7cfa85c82dee2baa0
m1thr4nd1r <vitocr_santos@hotmail.com>
2013-10-09 19:39:01 -0300

    commeting

--- random.py
+++ random.py
@@ -2,7 +2,7 @@ import os
 import random
 from flask import Flask
 from flask import render_template
-from flask import request
+# from flask import request
 
 app = Flask(__name__)
 app.debug = True  # Activating Debug of the App
@@ -16,22 +16,22 @@ def hello():
 def result():
 	
 	# If inside the result path, then do the sorting and render the template (with the data)
-	if request.method == 'POST':
+	# if request.method == 'POST':
 		
-		elements = request.values.getlist('element') # Gets all values of tags named 'element'
-		elements_bck = elements
-		things = request.values.getlist('thing') # Same as before, for tags named 'thing'
-		result = []
+	# 	elements = request.values.getlist('element') # Gets all values of tags named 'element'
+	# 	elements_bck = elements
+	# 	things = request.values.getlist('thing') # Same as before, for tags named 'thing'
+	# 	result = []
 
-		selector = -1
+	# 	selector = -1
 
-		while len(elements) > 0:
+	# 	while len(elements) > 0:
 			
-			pos = random.randint(0,len(elements) - 1)
-			selector = (selector + 1) % len(things)
-			result.append(elements.pop(pos) + " -> " + things[selector])
+	# 		pos = random.randint(0,len(elements) - 1)
+	# 		selector = (selector + 1) % len(things)
+	# 		result.append(elements.pop(pos) + " -> " + things[selector])
 		
-		return str(result)
+	# 	return str(result)
 
 if __name__ == '__main__':
     app.run()
