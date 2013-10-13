fd36e2779e70ade9257818df783c6d1a4ee67949
m1thr4nd1r <vitocr_santos@hotmail.com>
2013-10-12 23:04:11 -0300

    Add import to random

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
+import random
 from flask import Flask
 from flask import render_template
-from flask import request
 
 app = Flask(__name__)
 
@@ -12,20 +12,4 @@ def hello():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-	# If inside the result path, then do the sorting and render the template (with the data)
-	# if request.method == 'POST':
-		
-	# 	elements = request.values.getlist('element') # Gets all values of tags named 'element'
-	# 	elements_bck = elements
-	# 	things = request.values.getlist('thing') # Same as before, for tags named 'thing'
-	 	# result = []
-
-	# 	selector = -1
-
-	# 	while len(elements) > 0:
-			
-	# 		pos = random.randint(0,len(elements) - 1)
-	# 		selector = (selector + 1) % len(things)
-	# 		result.append(elements.pop(pos) + " -> " + things[selector])
-		
-	 	# return str(result)
+	return "chego"
