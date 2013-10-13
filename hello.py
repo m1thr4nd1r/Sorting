8dfc38e21c216709997555b032edd2389a360327
m1thr4nd1r <vitocr_santos@hotmail.com>
2013-10-12 23:08:25 -0300

    Add result route

--- hello.py
+++ hello.py
@@ -14,4 +14,20 @@ def hello():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-	return "chego"
+	# If inside the result path, then do the sorting and render the template (with the data)
+	if request.method == 'POST':
+		
+		elements = request.values.getlist('element') # Gets all values of tags named 'element'
+		elements_bck = elements
+		things = request.values.getlist('thing') # Same as before, for tags named 'thing'
+	 	result = []
+
+		selector = -1
+
+		while len(elements) > 0:
+			
+			pos = random.randint(0,len(elements) - 1)
+			selector = (selector + 1) % len(things)
+			result.append(elements.pop(pos) + " -> " + things[selector])
+		
+ 	return str(result)
